321a9ef756b511f7ed4a08ae6ed63f92d7c577c5
Khanh Vu <kvu9@lsu.edu>
2021-03-07 11:59:55 -0600

    add SearchToolForm

--- cooking_website/display/models.py
+++ cooking_website/display/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+price_range = (
+    ('under_5','Under $5'),
+    ('5_10','$5-$10'),
+    ('10-15','$10-$15'),
+)
+class SearchToolForm (forms.Form):
+    price_filter = forms.CharField(widget=forms.Select(choices=price_range))
--- cooking_website/display/views.py
+++ cooking_website/display/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SearchToolForm
 
 def Homepage(request):
     #return HttpResponse('This is homepage.')
     return render(request,'display/homepage.html/')
 def SearchTool(request):
-    return HttpResponse('This is search tool.')
-
+    #return HttpResponse('This is search tool.')
+    form = SearchToolForm()
+    return render(request,'display/search.html/', {'form':form})
 def RecipeSubmission(request):
     return HttpResponse('This is recipe submission page.')
 
