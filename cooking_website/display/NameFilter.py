f865c56fc066e481feabcebd6dd91316a24a03ba
Dustbowl <vuonganthony@hotmail.com>
2021-03-23 23:38:33 -0500

    Update NameFilter.py

--- cooking_website/display/NameFilter.py
+++ cooking_website/display/NameFilter.py
@@ -1,7 +1,10 @@
+#By Anthony Vuong
+#Inherited from Filter.py
 from Filter import Filter
 
 class NameFilter(Filter):
 
+    #filters through a single line for a name/key
     def search(self, line, hits):
 
         if(self._key != 'none'):
@@ -9,5 +12,7 @@ class NameFilter(Filter):
                  if self._key.lower() in line.lower():
 
                      hits += 100
+                    #Name is considered high relevance in a search engine so a large amount of 'weight' is given to it
 
         return hits
+        #returns the weight number
