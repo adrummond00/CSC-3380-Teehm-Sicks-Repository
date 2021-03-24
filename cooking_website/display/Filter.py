506379dc8db7697b28fc6301c9c75d635057a59d
Dustbowl <vuonganthony@hotmail.com>
2021-03-23 23:35:25 -0500

    Update Filter.py

--- cooking_website/display/Filter.py
+++ cooking_website/display/Filter.py
@@ -1,9 +1,11 @@
+#base class for the filters
+#by Anthony Vuong
 class Filter:
 
     def __init__(self, key):
 
         self._key = key
-
+        #key is the keyword being used to search
 
     def showKey(self):
         print(self._key)
