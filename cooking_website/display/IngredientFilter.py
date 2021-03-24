164cd41ca67362eca322d6bd290bad0df71ef3c4
Dustbowl <vuonganthony@hotmail.com>
2021-03-23 23:43:14 -0500

    Update IngredientFilter.py

--- cooking_website/display/IngredientFilter.py
+++ cooking_website/display/IngredientFilter.py
@@ -1,15 +1,22 @@
+#by Anthony Vuong
+#inherited from Filter.py
 from Filter import Filter
 
+#the key for this class is a list of strings (ingredients)
 class IngredientFilter(Filter):
 
+    #search line for all ingredients similarities
     def search(self, line, hits):
-
+        
+        #skips search if no ingredients in list
         if(len(self._key) > 0):
-
+            
+            #iterates and checks the line for each ingredient in the list
             for ingredient in range(len(self._key)):
 
                 if self._key[ingredient].lower() in line.lower():
 
                     hits += 1
-
+                    #low weight for each ingredient, but multiple ingredients could stack fairly quickly
         return hits
+        #returns weight number
