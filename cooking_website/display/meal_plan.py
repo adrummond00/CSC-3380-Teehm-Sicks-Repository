e1072306544c16ad3e5b6c69938e22db491d5d20
Ikaika <67708774+IkaikaL@users.noreply.github.com>
2021-03-29 15:24:22 -0500

    added meals

--- cooking_website/display/meal_plan.py
+++ cooking_website/display/meal_plan.py
@@ -7,3 +7,27 @@ daily_plan = {
     'Friday':'',
     'Saturday':'',
 }
+
+daily_meals = {
+    'SundayBreakfast':'',
+    'SundayLunch':'',
+    'SundayDinner':'',
+    'MondayBreakfast':'',
+    'MondayLunch':'',
+    'MondayDinner':'',
+    'TuesdayBreakfast':'',
+    'TuesdayLunch':'',
+    'TuesdayDinner':'',
+    'WednesdayBreakfast':'',
+    'WednesdayLunch':'',
+    'WednesdayDinner':'',
+    'ThursdayBreakfast':'',
+    'ThursdayLunch':'',
+    'ThursdayDinner':'',
+    'FridayBreakfast':'',
+    'FridayLunch':'',
+    'FridayDinner':'',
+    'SaturdayBreakfast':'',
+    'SaturdayLunch':'',
+    'SaturdayDinner':'',
+}
